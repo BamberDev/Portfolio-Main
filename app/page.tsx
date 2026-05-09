c2201afe72644ba5972fa63917782cd24258f3da
Bamberek <kabaczek99@gmail.com>
2026-05-09 18:25:06 +0200

    style: change homepage layout and text styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Smile } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center text-center text-[clamp(3rem,8vw,8rem)] bg-[#282828] min-h-screen">
-        <h1>COMING SOON</h1>
+      <div className="flex flex-col items-center justify-center text-center leading-none text-white text-[clamp(3rem,8vw,8rem)] bg-[#282828] min-h-screen">
+        <h1>
+          COMING
+          <br />
+          SOON
+        </h1>
+        <span className="[&>svg]:w-[clamp(3rem,4vw,5rem)] [&>svg]:h-[clamp(2rem,4vw,5rem)]">
+          <Smile />
+        </span>
       </div>
     </main>
   );
